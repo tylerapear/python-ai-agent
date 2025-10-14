a91fcd7fe14c9c223eafc1d0278f309c8936afff
Tyler Pearson <tyler.a.pear@gmail.com>
2025-10-13 19:53:06 -0700

    adds write_file and run_python_file functions for agent

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,4 @@
-import os
+import os, subprocess
 
 def get_files_info(working_directory, directory = ""):
   
@@ -21,4 +21,73 @@ def get_files_info(working_directory, directory = ""):
   
   except Exception as e:
     return f'Error: {e}'
+    
+def get_file_content(working_directory, file_path):
+  try:
+    full_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not full_path.startswith(os.path.abspath(working_directory)):
+      return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory\n'
+    if not os.path.isfile(full_path):
+      return f'Error: File not found or is not a regular file: "{file_path}"\n'
+    
+    MAX_CHARS = 10000
+    
+    with open(full_path, "r") as f:
+      check_string = f.read()
+      
+    
+    if len(check_string) > MAX_CHARS:
+      with open(full_path, "r") as f:
+        file_content_string = f'{f.read(MAX_CHARS)} [...File "{file_path}" truncated at 10000 characters]'
+    else:
+      file_content_string = check_string
+    return file_content_string
+
+  except Exception as e:
+    return f'Error: {e}'
+
+def write_file(working_directory, file_path, content):
+  try:
+    full_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not full_path.startswith(os.path.abspath(working_directory)):
+      return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory\n'
+    if not os.path.isfile(full_path):
+      open(full_path, "w").close()
+      
+    with open(full_path, "w") as f:
+      f.write(content)
+      
+    return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+      
+  except Exception as e:
+    return f'Error: {e}'
+  
+def run_python_file(working_directory, file_path, args=[]):
+  try:
+    abs_working_dir = os.path.abspath(working_directory)
+    full_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not full_path.startswith(os.path.abspath(working_directory)):
+      return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory\n'
+    if not os.path.isfile(full_path):
+      return f'Error: File "{file_path}" not found.\n'
+    if not file_path.endswith(".py"):
+      return f'Error: "{file_path}" is not a Python file.\n'
+    
+    
+    commands = ["python", full_path]
+    if args:
+      commands.extend(args)
+
+    response = ""
+    completed_process = subprocess.run(commands, timeout=30, capture_output=True, text=True, cwd=abs_working_dir)
+    if not completed_process:
+      return "No output produced\n"
+    response += f"STDOUT: {completed_process.stdout}\n"
+    response += f"STDERR: {completed_process.stderr}\n"
+    if completed_process.returncode:
+      response += f"Process exited with code {completed_process.returncode}\n"
+    return response
+    
+  except Exception as e:
+    return f"Error: executing Python file: {e}\n"
     
--- tests.py
+++ tests.py
@@ -1,7 +1,6 @@
-from functions.get_files_info import get_files_info
+from functions.get_files_info import get_files_info, get_file_content, write_file, run_python_file
 
-def main():
-  
+def test_dirs():
   test1_results = get_files_info("calculator", ".")
   print(f'Results from current directory:')
   print(test1_results)
@@ -17,6 +16,65 @@ def main():
   test4_results = get_files_info("calculator", "../")
   print(f"Results from '../' directory:")
   print(test4_results)
+  
+def test_files():
+  test1_results = get_file_content("calculator", "main.py")
+  print(f'Results from main.py:')
+  print(test1_results)
+  
+  test2_results = get_file_content("calculator", "pkg/calculator.py")
+  print(f"Results from 'pkg/calculator':")
+  print(test2_results)
+  
+  test3_results = get_file_content("calculator", "/bin/cat")
+  print(f"Results from '/bin/cat':")
+  print(test3_results)
+  
+  test4_results = get_file_content("calculator", "pkg/does_not_exist.py")
+  print(f"Results from 'pkg/does_not_exist.py':")
+  print(test4_results)
+
+def test_write():
+  test1_results = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+  print(f'Results from test1:')
+  print(test1_results)
+  
+  test2_results = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+  print(f"Results from test2:")
+  print(test2_results)
+  
+  test3_results = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+  print(f"Results from test3:")
+  print(test3_results)
+  
+def test_run():
+  test1_results = run_python_file("calculator", "main.py")
+  print(f'Results from test1:')
+  print(test1_results)
+  
+  test2_results = run_python_file("calculator", "main.py", ["3 + 5"])
+  print(f"Results from test2:")
+  print(test2_results)
+  
+  test3_results = run_python_file("calculator", "tests.py")
+  print(f"Results from test3:")
+  print(test3_results)
+  
+  test4_results = run_python_file("calculator", "../main.py")
+  print(f"Results from test4:")
+  print(test4_results)
+  
+  test5_results = run_python_file("calculator", "nonexistent.py")
+  print(f"Results from test5:")
+  print(test5_results)
+  
+  test6_results = run_python_file("calculator", "lorem.txt")
+  print(f"Results from test6:")
+  print(test6_results)
+
+def main():
+  test_run()
+  
   
   
   
